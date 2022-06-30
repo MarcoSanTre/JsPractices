a975848b722053c8e5ffe0430035e828acdede6d
marcosantre <marco.sanchez@outlook.com>
2022-06-29 22:10:59 -0500

    Sesion 1 commit

---
+++ Ses01_220629/js/base.js
@@ -0,0 +1,23 @@
+// Datos en Js
+    // Identificador
+    //Reglas o buenas prácticas
+    //Los identificadores No inician con número
+    //let 9variable = 9
+    //No acepta palabras reservadas
+    //let else = 'else' ==> error
+    //Sensible a Mayus y minus
+    //Buena práctica ==> camelCase 99%
+    // ==> PascalCase -> Clases
+    // Naming descriptivo
+    //Reglas: No debe contener caractéres especiales
+
+let edadKoder = 25;
+var alturaKoder = 165;
+
+console.log(edadKoder);
+console.log(alturaKoder);
+
+
+
+
+
