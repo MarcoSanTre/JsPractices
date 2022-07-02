9b95684a8970b9005dd7a777b1bc8126aee9a149
marcosantre <marco.sanchez@outlook.com>
2022-07-01 22:05:23 -0500

    Practice 3 commit (Practice 2, 3 and 4)

--- js/conditionals.js
+++ js/conditionals.js
@@ -15,15 +15,39 @@
 //    console.log('MSI no está disponible')
 //}
 
-let num1 = prompt('Ingresa 1 número')
-let num2 = prompt('Ingresa el 2o número')
-let num3 = prompt('Ingresa el 3er número')
+// let num1 = prompt('Ingresa 1 número')
+// let num2 = prompt('Ingresa el 2o número')
+// let num3 = prompt('Ingresa el 3er número')
 
-if(num1 > num2 && num1 > num3) {
-    console.log('El mayor es el ' + num1)
-} if(num2 > num1 && num2 > num3) {
-    console.log('El mayor es el ' + num2)
-}
-if(num3 > num1 && num3 > num2) {
-    console.log('El mayor es el ' + num3)
-}
+// if(num1 > num2 && num1 > num3) {
+//     console.log('El mayor es el ' + num1)
+// } if(num2 > num1 && num2 > num3) {
+//     console.log('El mayor es el ' + num2)
+// }
+// if(num3 > num1 && num3 > num2) {
+//     console.log('El mayor es el ' + num3)
+// }
+
+// Práctica 2
+
+// let num1 = prompt('Ingresa el 1er número')
+// let num2 = prompt('Ingresa el 2o número')
+
+// let mayor = num1 > num2 ? 'El primer número es mayor' : 'El segundo es mayor'
+// console.log( mayor)
+
+//Práctica 3
+
+// let num1 = prompt('Ingresa el 1er número')
+// let num2 = prompt('Ingresa el 2o número')
+// let num3 = prompt('Ingresa el 3er número')
+// let mayor = num1 > num2 && num1 > num3 ? '1o es mayor' : num2 > num1 && num2 > num3 ? '2o es mayor' : '3o es mayor'
+// console.log(mayor)
+
+//Práctica 4
+
+let num1 = parseInt(prompt('Ingresa el 1er número'))
+let num2 = parseInt(prompt('Ingresa el 2o número'))
+
+let resultado = num1 > num2 ? num1 / num2 : num2 > num1 ? num1 + num2 : num1 = num2 ? num1 * num2 : 'Error'
+console.log(resultado)
