c4a070d27b8e4b329b8554e479a98baa8ca8d7cd
marcosantre <marco.sanchez@outlook.com>
2022-07-04 17:24:26 -0500

    Homework commit (par and triangle area)

---
+++ js/homework.js
@@ -0,0 +1,18 @@
+// Tarea 1
+
+// let num = parseInt(prompt('Ingresa un número'))
+
+// if (num % 2 == 0)
+//   {
+//     console.log('El número introducido es par');
+//   }
+//   else {console.log('El número introducido es impar');
+// }
+
+
+//Tarea 2
+
+// let base = prompt('Ingresa la base')
+// let height = prompt('Ingresa la altura')
+// let area = (base * height) / 2
+// console.log('El área es de '+ area)
