5474487ac8fd0f823514b99771c138c117677f16
marcosantre <marco.sanchez@outlook.com>
2022-07-05 19:37:04 -0500

    String Methods homework commit

--- js/stringMethods.js
+++ js/stringMethods.js
@@ -1,4 +1,10 @@
-let message = "I still don't know JavaScript"
-let result = message.startsWith("I");
-console.log(result)
+// let message = "I still don't know JavaScript"
+// let result = message.startsWith("I still");
+// console.log(result)
 
+let userMessage = prompt('Escribe lo que gustes');
+let messageLength = userMessage.length;
+let vocalCounter = userMessage.match(/[aeiou]/gi).length;
+let wordsCounter = userMessage.split(" ");
+
+console.log(`Tu frase tiene ${messageLength} caracteres, contiene ${vocalCounter} vocales y las palabras encontradas son ${wordsCounter}`)
