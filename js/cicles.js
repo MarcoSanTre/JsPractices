bd7f77e6f194fb489744e395a8e2a1208f2bbeb1
marcosantre <marco.sanchez@outlook.com>
2022-07-06 22:19:09 -0500

    Functions commit

---
+++ js/cicles.js
@@ -0,0 +1,52 @@
+// for(let i = 0 ; i < 3 ; i++) {
+// 	console.log('Esto es una iteración')
+// }
+
+// for (let i = 0 ; i < 3 ; i++) {
+// 	console.log(`La cuenta va en ${i}`);
+// }
+
+// let j = 0
+// while(j < 3) {
+// 	console.log(j)
+// 	j++
+// }
+
+// let j = 0
+// while(j <= 10) {
+// 	console.log(`5 X ${j} = ${j * 5}`)
+// 	j++
+// }
+
+// let k = 0
+// do{
+// 	console.log(`5 X ${k} = ${k * 5}`)
+// 	k++
+// } while (k <= 10)
+
+
+// Práctica 1
+
+
+
+// Práctica 2
+
+
+
+// Práctica 3
+
+// let k = 0
+// let i = parseInt(prompt('Ingresa un número'));
+// while(k <= 10) {
+// 	console.log(`${i} X ${k} = ${i * k}`)
+// 	k++
+// }
+
+// Ejercicio 1
+// 	• Pedir el usuario un número entre 10 y 100
+// 	• * Imprimir todos los números pares que existen entre 1 y ese número
+// 	• * p.ej. 12 -> 2,4,6,8,10,12
+// 	• * Usar loops, condicionales...
+// hint -> %
+
+let i = parseInt(prompt('Ingresa un número entre 10 y 100'));
---
+++ js/functions.js
@@ -0,0 +1,30 @@
+// function funcionSuma()  {
+// 	console.log('Esto es una funcion');
+// }
+
+// funcionSuma();
+
+// function funcionAreaTrian () {
+//     let base = prompt('Ingresa la base')
+//     let height = prompt('Ingresa la altura')
+//     let area = (base * height) / 2
+// console.log('El área es de '+ area)
+// }
+// funcionAreaTrian();
+
+// let b = prompt('Ingresa la base')
+// let h = prompt('Ingresa la altura')
+
+// function funcionAreaTrian (base, height) {
+//     let area = (base * height) / 2
+// return console.log(area)
+// }
+
+// funcionAreaTrian(10,12);
+
+let a = 10;
+function suma(num1, num2=10){
+	let a = num1 + num2;
+	return a
+}
+console.log(suma(a));
