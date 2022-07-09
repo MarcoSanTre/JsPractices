154aa0771ca226a460686045ae2cc2ce609f1ba8
marcosantre <marco.sanchez@outlook.com>
2022-07-08 22:03:33 -0500

    Arrays commit

---
+++ js/arrays.js
@@ -0,0 +1,10 @@
+// const koders = ["Mariana","Marco","Cris"];
+// console.log(koders);
+
+let prime_numbers = [2, 3, 5, 7, 9, 11];
+
+// replace 1 element from index 4 by 13
+let removedElement = prime_numbers.splice(4, 1, 13);
+console.log(removedElement);
+console.log(prime_numbers);
+
