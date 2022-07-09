28a7b1837a0540c958f5e31b6584eb4437e22b93
marcosantre <marco.sanchez@outlook.com>
2022-07-08 20:56:43 -0500

    Arrow Functions second commit

--- js/arrow_function.js
+++ js/arrow_function.js
@@ -1,5 +1,21 @@
-const arrowFunction = (p1, p2) => {
-    console,log(p1, p2)
-    return
-};
+// const arrowFunction = (p1) => {
+//     console,log(p1);
+//     return
+// };
 
+const palabra = () => {
+    let palabra = prompt("Please type a word or sentence");
+    let sinEspacios = palabra.replaceAll(" ", "");
+    let k = sinEspacios.length;
+    let invertida = '';
+    for (let i = (k - 1); i >= 0; i--) {
+        invertida += sinEspacios[i];
+    }
+    if (sinEspacios === invertida) {
+        return 'Es palindromo'
+    }
+    return 'No es palindromo'
+}
+alert(palabra())
+
+    
