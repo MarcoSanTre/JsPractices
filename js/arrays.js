855c753c230a6e90504f94f0c53672fc0c6a4961
marcosantre <marco.sanchez@outlook.com>
2022-07-12 20:45:32 -0500

    Third Arrays commit

--- js/arrays.js
+++ js/arrays.js
@@ -47,12 +47,55 @@
 
 // console.log(arr);
 
-let arrayCities = ['méxicO','RIo', 'Los AngelEs', 'esTAMBUL'];
-let newCities = [];
-const capitalize = (arrayCities) => {
-    newCities = arrayCities.map(element => {
-        return(element[0].toUpperCase() + element.slice(1).toLowerCase());
-    });
-}
-capitalize(arrayCities);
-console.log(newCities);
+// let arrayCities = ['méxicO','RIo', 'Los AngelEs', 'esTAMBUL'];
+// let newCities = [];
+// const capitalize = (arrayCities) => {
+//     newCities = arrayCities.map(element => {
+//         return(element[0].toUpperCase() + element.slice(1).toLowerCase());
+//     });
+// }
+// capitalize(arrayCities);
+// console.log(newCities);
+
+// let arr = [1,2,3,3,4,5]
+
+//     const cbFun = (acumualudar, cv) => {
+//       acumualudar +=  cv
+//       return acumualudar
+//   }
+
+//     let result = arr.reduce(cbFun, 1000) 
+
+//     console.log(result)
+
+//     const koders = ['kelly', 'sebas','Adrian']
+
+//     result = koders.reduce((acc, cv) => {
+//       acc.push(cv[0]);
+//       return acc;
+//      }, []);
+
+
+//      result2 = koders.reduce((acc, cv) => {
+//        acc += `${cv} `;
+//        return acc;
+//       }, '');
+     
+
+//     console.log(result)
+//     console.log(result2)
+
+// Práctica
+
+// Función que reciba un array de nombre y esta debe devolver un string con las iniciales de los nombre
+ 
+// getInitial(['Ferdinand','Jose','Maria','Dora']) -> FJMD
+
+const koders = ['Ferdinand','Jose','Maria','Dora']
+
+result = koders.reduce((acc, cv) => {
+    return (acc += cv[0]);
+    }, "");
+
+console.log(result)
+
